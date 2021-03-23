73533fb408189c0ec500a8c6d436c638dfd4e365
boomt1337 <lol558637@gmail.com>
2021-03-24 10:29:16 +1300

    initial commit
    work done on 24 march 2021
    
    objectives met for today:
    getting the skeleton of the website operational

---
+++ magswebreal/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--Copyright Heeju Shim 2021. All rights reserved. !-->
+<!-- code for loading fonts as well as a viewport to fit mobile and smaller screens !-->
+<meta charset="utf-8">
+<link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Arvo&family=Roboto+Slab&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device,width, initial-scale=1.0"> 
+<link rel=stylesheet href="css/style.css"/>
+<title>Tourism in New Zealand</title>
+<html>
+<!--main content !-->
+<div class="wrapper">
+    <?php include 'src/top.php'; ?>
+    <header class="box">
+    <h1>test</h1>
+    </header>
+    </div>
+</html>
+
+
